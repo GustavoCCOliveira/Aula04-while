edbc502011a273c7af3b97e72544d4b648521bb6
GustavoCCOliveira <gustavotorresdeoliveira20@gmail.com>
2026-04-28 10:48:42 -0300

    d

---
+++ While_teste.py
@@ -0,0 +1,21 @@
+cp = 0
+while cp < 10:
+   cp += 1
+
+
+   if cp == 3:
+       continue
+
+
+   print(f"produto {cp}")
+
+
+
+
+   #while decrescente de 4 até 1
+
+
+i = 4
+while i > 0:
+   print(i)
+   i -= 1
